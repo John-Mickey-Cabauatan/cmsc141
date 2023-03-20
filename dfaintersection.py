20892b0714e233520f50ae6ed0f07351933a9cc4
Ji Sison <jisison.dev@gmail.com>
2023-03-20 22:15:36 +0800

    set new states and alphabet of dfa_intersection

--- dfaintersection.py
+++ dfaintersection.py
@@ -5,12 +5,19 @@ Name:   John Mickey Cabauatan
         Jianne Rimar Sison
 Date: 27 March 2023
 """
-
+import itertools
 from dfa import DFA
 
-def dfa_intersection(self, M = DFA()):
 
-    
+def dfa_intersection(self, M = DFA()):
+    """
+    Input two DFAs
+    """
+    states = set(itertools.product(self.states,M.states))
+    alphabet = self.alphabet & M.alphabet
+
+
+
     dfa_intersection  = DFA(
         
     )
