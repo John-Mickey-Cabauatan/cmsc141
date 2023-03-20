d81e2cd85a16473652f4fd29299dff02ba65397d
John-Mickey-Cabauatan <jmcabauatan@up.edu.ph>
2023-03-20 19:10:52 +0800

    working

--- dfacomplement.py
+++ dfacomplement.py
@@ -8,7 +8,7 @@ Date: 27 March 2023
 
 from dfa import DFA
 
-def dfa_complement(self, M):
+def dfa_complement(self):
     str_complement = DFA(states = self.states, alphabet = self.alphabet, 
         transition = self.transition, start_state = self.start_state, 
             accept_states = self.states.difference(self.accept_states))
