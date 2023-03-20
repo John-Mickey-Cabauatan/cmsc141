9c5cc0e08128ab2f3d6b55c5dea48e86f507daf8
Ji Sison <jisison.dev@gmail.com>
2023-03-20 21:44:42 +0800

    initial commit dfaintersection

---
+++ dfaintersection.py
@@ -0,0 +1,17 @@
+"""
+CMSC 141 X Exercise 1
+Name:   John Mickey Cabauatan
+        Cedric Ramos
+        Jianne Rimar Sison
+Date: 27 March 2023
+"""
+
+from dfa import DFA
+
+def dfa_intersection(self, M = DFA()):
+
+    
+    dfa_intersection  = DFA(
+        
+    )
+    return dfa_intersection
