080d7f096d4ed2198050827ae955f5233693efc0
Ji Sison <jisison.dev@gmail.com>
2023-03-21 16:20:52 +0800

    return type fiixed

--- dfacomplement.py
+++ dfacomplement.py
@@ -9,8 +9,10 @@ Date: 27 March 2023
 from dfa import DFA
 
 def dfa_complement(self):
-    str_complement = DFA(states = self.states, alphabet = self.alphabet, 
+    dfa_complement = DFA(states = self.states, alphabet = self.alphabet, 
         transition = self.transition, start_state = self.start_state, 
             accept_states = self.states.difference(self.accept_states))
-
-    return "\n...For DFA Complement...\n" + f"{str_complement}\n"
+    
+    return dfa_complement
+    #return "\n...For DFA Complement...\n" + f"{str_complement}\n" 
+    # I changed the code, need to return DFA class - ji
